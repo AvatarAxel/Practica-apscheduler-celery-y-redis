64351f7d169e4716cfe233942d037c1b21187e92
Axel <axel@iMac-de-Kuantik.local>
2025-03-13 15:11:04 -0600

    celery and redis integration

--- app/main.py
+++ app/main.py
@@ -1,18 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from celery.result import AsyncResult
 from datetime import datetime
-from app.task import print_example
+from app.task import print_example, task_de_prueba, app as celery_app
+import pytz
 
 app = FastAPI()
 
 @app.on_event("startup")
 def start_scheduler():
-    # Date YYYY/MM/DD hh/mm/ss
-    run_date = datetime(2025, 3, 12, 11, 5, 1)
-    print("Tii")
-    print(datetime.now())
-    print_example.apply_async(eta=run_date)
+    cdmx_tz = pytz.timezone("America/Mexico_City")
+    run_date = datetime(2025, 3, 12, 14, 15, 0, tzinfo=cdmx_tz)
+    print("Mi hora que configuro:", run_date)
+    print("La hora actual en mi zona horaria:", datetime.now(cdmx_tz))
+    print("Tarea programada: ", print_example.apply_async(eta=run_date))
+    print("Tarea de prueba: ", task_de_prueba.apply_async())
 
 @app.get("/")
 def read_root():
     print("uwu")
-    return {"Onichan": "uwu, yiyi"}
+    return {"Onichan": "uwu"}
+
+@app.get("/task-result/{task_id}")
+def get_task_result(task_id: str):
+    task_result = AsyncResult(task_id, app=celery_app)
+    if task_result.ready():
+        return {"task_id": task_id, "status": task_result.status, "result": task_result.result}
+    else:
+        return {"task_id": task_id, "status": task_result.status}
--- app/task.py
+++ app/task.py
@@ -1,15 +1,21 @@
 from celery import Celery
+import requests
 
-# Configura Celery para usar Redis
-app = Celery('my_tasks', broker='redis://redis:6379/0')
+app = Celery('my_tasks', 
+             broker='redis://redis:6379/0',
+             backend='redis://redis:6379/0')
 
 @app.task
 def get_example():
-    import requests
     resultRequest = requests.get('https://w3schools.com/python/demopage.htm')
-    print(resultRequest.text)
+    return resultRequest.text
 
 @app.task
 def print_example():
     print("Comienza la serie de peticiones:")
     get_example.apply_async(countdown=2)
+
+@app.task
+def task_de_prueba():
+    resultRequest = requests.get('https://w3schools.com/python/demopage.htm')
+    return resultRequest.text
