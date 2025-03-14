dd9287314ed734baa6f796420846d7b90630230a
Axel <axel@iMac-de-Kuantik.local>
2025-03-14 13:01:37 -0600

    Updates

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -1,7 +1,18 @@
 from celery import Celery
+import requests
 
 celery_app = Celery('worker', broker='redis://localhost:6379/0')
 
 @celery_app.task
 def sumar(x, y):
     return x + y
+
+@celery_app.task
+def petition():
+    resultRequest = requests.get('https://w3schools.com/python/demopage.htm')
+    return resultRequest.text
+
+@celery_app.task
+def petition_to_time(time_received):
+    resultRequest = requests.get('https://w3schools.com/python/demopage.htm')
+    return resultRequest.text
--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
-from celery_worker import sumar
+from celery_worker import sumar, petition, petition_to_time
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "¡Hola, FastAPI y Celery!"}
+    return {"Hello": "Mundo mundial"}
 
 @app.get("/sumar/{x}/{y}")
 async def sumar_task(x: int, y: int):
     result = sumar.apply_async((x, y))
+    return {"task_id": result.id, "status": "task started"}
+
+@app.get("/petition")
+async def petition_task():
+    result = petition.apply_async(())
+    return {"task_id": result.id, "status": "task started"}
+
+@app.post("/petition_to_time/{time_received}")
+async def petition_task(time_received: int):
+    result = petition_to_time.apply_async((time_received,))
     return {"task_id": result.id, "status": "task started"}
